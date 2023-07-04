1c20b1517ad6d54aa993f3865f27004756082548
RicardoAguilera92 <ricardo.aguilera92@icloud.com>
2023-07-04 12:30:30 -0600

    Componente de Layout

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,5 +1,7 @@
+import Layout from '../Components/Layout'
+
 function SignIn() {
-    return <div className="bg-red-100">SignIn</div>;
+  return <Layout >Sing In</Layout>;
   }
   
   export default SignIn;
