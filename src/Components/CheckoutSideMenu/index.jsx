66031454f98dad306286de6da2b23ce03aec4eca
RicardoAguilera92 <ricardo.aguilera92@icloud.com>
2023-07-05 14:00:16 -0600

    Eliminar productos del carrito

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -6,6 +6,12 @@ import { XMarkIcon } from "@heroicons/react/24/solid";
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext);
+  const handleDelete = (id) => {
+    const filteredProducts = context.cartProducts.filter(
+      (product) => product.id != id
+    );
+    context.setCartProducts(filteredProducts);
+  };
 
   return (
     <aside
@@ -22,15 +28,18 @@ const CheckoutSideMenu = () => {
           />
         </div>
       </div>
-      ,<div className="px-6 overflow-y-scroll">
-      {context.cartProducts.map((product) => (
-        <OrderCard
-          key={product.id}
-          title={product.title}
-          imageUrl={product.image}
-          price={product.price}
-        />
-      ))}
+      ,
+      <div className="px-6 overflow-y-scroll">
+        {context.cartProducts.map((product) => (
+          <OrderCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            imageUrl={product.image}
+            price={product.price}
+            handleDelete={handleDelete}
+          />
+        ))}
       </div>
     </aside>
   );
