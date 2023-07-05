abf6edfde9434326ecd7aaaea5a40580fdfea679
RicardoAguilera92 <ricardo.aguilera92@icloud.com>
2023-07-05 08:47:26 -0600

    Heroicons con TailwindCSS

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,13 +1,15 @@
+import { useContext } from "react";
+import { ShoppingCartIcon } from '@heroicons/react/24/solid'
 import { NavLink } from "react-router-dom";
-import {useContext} from "react";
-import {ShoppingCartContext} from "../../Context";
+import { ShoppingCartContext } from "../../Context";
+
 
 const Navbar = () => {
   const context = useContext(ShoppingCartContext);
   const activeStyle = "underline underline-offset-4";
 
   return (
-    <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
+    <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light bg-white">
       <ul className="flex items-center gap-3">
         <li className="font-semibold text-lg">
           <NavLink to="/">Shopi</NavLink>
@@ -87,7 +89,11 @@ const Navbar = () => {
             Sign In
           </NavLink>
         </li>
-        <li>🛒 {context.count}</li>
+        <li className="flex items-center">
+        <ShoppingCartIcon className="h-6 w-6 text-black-500 " />
+        <div>{context.count}</div>
+          
+        </li>
       </ul>
     </nav>
   );
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,24 +1,15 @@
 import "./styles.css";
+import { XMarkIcon } from '@heroicons/react/24/solid'
 
 const ProductDetail = () => {
   return (
     <aside className="product-detail flex flex-col fixed right-0 border border-black rounded bg-white">
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
+        <div>
+      <XMarkIcon className="h-6 w-6 text-black-500" />
+     
+    </div>
       </div>
     </aside>
   );
